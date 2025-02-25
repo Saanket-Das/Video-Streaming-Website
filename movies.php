49a2bf18209364e80613c533daff765f047aaa9f
Sanket <98513140+Saanket-Das@users.noreply.github.com>
2025-02-25 19:35:35 +0530

    Add files via upload

---
+++ movies.php
@@ -0,0 +1,140 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movie Trailer website</title>
+    <link rel="stylesheet" href="css/style3.css"/>
+    <link rel="stylesheet" href="css/lightslider.css"/>
+    <script src="js/JQuery3.3.1.js"></script>
+    <script src="js/lightslider.js"></script>
+    <link rel="shortcut icon" href="images/fav icon.png"/>
+    <script src="https://kit.fontawesome.com/c8e4d183c2.js" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <nav>
+            <a href="#" class="logo">
+                <img src="images/Popcorn-logos_black.png"width="30" height="40">
+            </a>
+            <ul class="menu">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="movies.php">Movies</a></li>
+                <li><a href="tvshows.php">TV shows</a></li>
+                <li><a href="logout.php">Logout</a></li>
+            </ul>
+            <div class="search">
+                <input type="text" placeholder="Find your favourite Movies"/>
+                <i class="fas fa-search"></i>
+            </div>
+        </nav>
+    <section id="main">
+        <h1 class="showcase-heading">Movie Trailer</h1>
+        <ul id="autoWidth" class="cs-hidden">
+        <li class="item-p">
+            <div class="showcase-box"><a href="https://youtu.be/Go8nTmfrQd8">
+                <img src="images/m-22.jpg"></a>
+                </div>
+            </li>
+
+            <li class="item-e">
+                            <div class="showcase-box"><a href="https://www.youtube.com/watch?v=hEJnMQG9ev8">
+                                <img src="images/s-5.png"></a>
+                                </div>
+                          
+            
+            
+            <li class="item-b">
+                <div class="showcase-box"><a href="https://www.youtube.com/watch?v=Q0CbN8sfihY">
+                    <img src="images/s-2.jpg"></a>
+                    </div>
+                
+                <li class="item-c">
+                    <div class="showcase-box"><a href="https://www.youtube.com/watch?v=e3Nl_TCQXuw">
+                        <img src="images/s-3.jpg"></a>
+                        </div>
+                    
+                    <li class="item-d">
+                        <div class="showcase-box"><a href="https://www.youtube.com/watch?v=cSp1dM2Vj48">
+                            <img src="images/s-4.jpg"></a>
+                            </div>
+                        </li>
+                    
+
+                            <div class="showcase-box"><a href="https://www.youtube.com/watch?v=QFxN2oDKk0E">
+                <img src="images/s-1.jpg">></a>
+                </div>
+          </ul>
+    
+    
+          <li class="item-f">
+            <div class="showcase-box"><a href="https://youtu.be/neY2xVmOfUM">
+                <img src="images/m-10.jpg"></a>
+                </div>
+            </li>
+
+             <li class="item-g">
+            <div class="showcase-box"><a href="https://youtu.be/YRSBiTF3wNw">
+                <img src="images/m-11.jpg"></a>
+                </div>
+            </li>
+
+              <li class="item-h">
+            <div class="showcase-box"><a href="https://youtu.be/zyYgDtY2AMY">
+                <img src="images/m-12.jpg"></a>
+                </div>
+            </li>
+
+              <li class="item-i">
+            <div class="showcase-box"><a href="https://youtu.be/DtQycgMD4HQ">
+                <img src="images/m-13.jpg"></a>
+                </div>
+            </li>
+
+            <li class="item-j">
+            <div class="showcase-box"><a href="https://youtu.be/T3tidwW1gGM">
+                <img src="images/m-14.jpg"></a>
+                </div>
+            </li>
+
+             <li class="item-k">
+            <div class="showcase-box"><a href="https://youtu.be/Salz7uGp72c">
+                <img src="images/m-15.jpg"></a>
+                </div>
+            </li>
+
+             <li class="item-L">
+            <div class="showcase-box"><a href="https://youtu.be/9sF8kWK4hoI">
+                <img src="images/m-16.jpg"></a>
+                </div>
+            </li>
+
+             <li class="item-M">
+            <div class="showcase-box"><a href="https://youtu.be/2m1drlOZSDw">
+                <img src="images/m-17.jpg"></a>
+                </div>
+            </li>
+
+             <li class="item-N">
+            <div class="showcase-box"><a href="https://youtu.be/C0BMx-qxsP4">
+                <img src="images/m-18.jpg"></a>
+                </div>
+            </li>
+
+            <li class="item-o">
+            <div class="showcase-box"><a href="https://youtu.be/MVt32qoyhi0">
+                <img src="images/m-19.jpg"></a>
+                </div>
+            </li>
+
+             <li class="item-p">
+            <div class="showcase-box"><a href="https://youtu.be/giXco2jaZ_4">
+                <img src="images/m-20.jpg"></a>
+                </div>
+            </li>
+
+            
+</body>
+   </html>
+
---
+++ tvshows.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movie Trailer website</title>
+    <link rel="stylesheet" href="css/style2.css"/>
+    <link rel="stylesheet" href="css/lightslider.css"/>
+    <script src="js/JQuery3.3.1.js"></script>
+    <script src="js/lightslider.js"></script>
+    <link rel="shortcut icon" href="images/fav icon.png"/>
+    <script src="https://kit.fontawesome.com/c8e4d183c2.js" crossorigin="anonymous"></script>
+    <link rel="shortcut icon" href="images/fav icon.png"/>
+    </head>
+    <body>
+        <nav>
+            <a href="#" class="logo">
+                <img src="images/Popcorn-logos_black.png"width="30" height="40">
+            </a>
+            <ul class="menu">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="movies.php">Movies</a></li>
+                <li><a href="tvshows.php">TV shows</a></li>
+                <li><a href="logout.php">Logout</a></li>
+    
+            </ul>
+            <div class="search">
+                <input type="text" placeholder="Find your favourite Movies"/>
+                <i class="fas fa-search"></i>
+            </div>
+        </nav>
+    <section id="main">
+
+        <h1 class="showcase-heading">Tv shows Trailer</h1>
+
+        <ul id="autoWidth" class="cs-hidden">
+            <li class="item-a">
+            <div class="showcase-box"><a href="https://www.youtube.com/watch?v=yQEondeGvKo">
+                <img src="images/s-6.jpg">></a>
+                </div>
+            </li>
+            <li class="item-b">
+                <div class="showcase-box"><a href="
+                    https://www.youtube.com/watch?v=rCj-Fb1OmXg">
+                    <img src="images/s-7.jpeg"></a>
+                    </div>
+                </li>
+                <li class="item-c">
+                    <div class="showcase-box"><a href="
+                        https://www.youtube.com/watch?v=sEOuJ4z5aTc">
+                        <img src="images/s-8.jpeg"></a>
+                        </div>
+                    </li>
+                    <li class="item-d">
+                        <div class="showcase-box"><a href="https://www.youtube.com/watch?v=LHOtME2DL4g
+                            ">
+                            <img src="images/s-9.jpg"></a>
+                            </div>
+                        </li>
+                        <li class="item-e">
+                            <div class="showcase-box"><a href="https://www.youtube.com/watch?v=LTpmw0Ac6Zs">
+                                <img src="images/s-10.jpeg"></a>
+                                </div>
+                            </li>
+          </ul>
+    </section>
+
+</body>
+</html>
